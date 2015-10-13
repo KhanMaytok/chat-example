1884db545690ff103813692aeba836e406437320
Giancarlo Ventura <sakya_stelios@hotmail.com>
2015-10-13 12:09:46 -0500

    Added post example

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+app.use(bodyParser.urlencoded({ extended: true }));
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res){
+  console.log(req.body);
+  res.json(req.body);
+  //res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
